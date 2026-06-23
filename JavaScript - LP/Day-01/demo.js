a9d2cb91850c44a535051d8f533b8f8736f537de
Prince S. Nayi <naiprince345@gmail.com>
2026-06-23 22:37:24 +0530

    Add files via upload

---
+++ JavaScript - LP/Day-01/demo.js
@@ -0,0 +1,19 @@
+console.log("Hello Demo file...!!");
+
+// DOM (Document Object Model) Method...
+
+/*
+
+    innerHTML
+    innerText
+    textContent
+
+*/
+
+a = 10
+b = 20
+c = a + b
+
+document.getElementById("heading").innerHTML = c;
+
+console.log(c);
